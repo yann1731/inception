e2dd421715fcbc64173c2085f1fe9bc78c79b8ec
yan <yan_1731@hotmail.com>
2023-02-21 13:59:02 -0500

    let's go

--- srcs/requirements/nginx/config/entrypoint.sh
+++ srcs/requirements/nginx/config/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/key.pem -out /etc/nginx/certs/cert.pem -subj "/C=US/ST=California/L=San Francisco/O=Example Inc./OU=IT Department/CN=localhost"
+openssl req -x509 -nodes -sha256 -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/key.pem -out /etc/nginx/certs/cert.pem -subj "/CN=yst-laur.42.fr/C=CA/ST=QC/L=Quebec/O=42Network/OU=42Quebec"
 nginx -g "daemon off;"
