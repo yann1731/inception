79fa03610cc03c40bab0dc0a67997b5d1cfb89e0
fenix144 <yan_1731@hotmail.com>
2023-02-28 14:58:49 -0500

    let's go

--- srcs/requirements/nginx/config/entrypoint.sh
+++ srcs/requirements/nginx/config/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-openssl req -x509 -nodes -sha256 -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/key.pem -out /etc/nginx/certs/cert.pem -subj "/CN=yst-laur.42.fr/C=CA/ST=QC/L=Quebec/O=42Network/OU=42Quebec"
+mkdir /etc/nginx/ssl/
+openssl req -x509 -nodes -sha256 -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/key.pem -out /etc/nginx/ssl/cert.pem -subj "/CN=yst-laur.42.fr/C=CA/ST=QC/L=Quebec/O=42Network/OU=42Quebec"
 nginx -g "daemon off;"
