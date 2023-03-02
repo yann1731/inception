d5be0621ec2445fa3897c18876a5d091c0d9aef5
yannick <yan_1731@hotmail.com>
2023-03-02 15:09:49 -0500

    Done??

--- srcs/requirements/nginx/config/entrypoint.sh
+++ srcs/requirements/nginx/config/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+
+if [-e configdone]; then
+
 sleep 6
 mkdir -p /etc/nginx/ssl/
 echo changing directory ownership...
@@ -8,7 +11,7 @@ echo changing directory ownership done...
 
 openssl req -x509 -nodes -sha256 -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/key.pem -out /etc/nginx/ssl/cert.pem -subj "/CN=yst-laur.42.fr/C=CA/ST=QC/L=Quebec/O=42Network/OU=42Quebec"
 
-
+fi
 
 echo launching nginx...
 nginx -g "daemon off;"
