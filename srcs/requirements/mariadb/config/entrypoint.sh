a45556e1594c3020a078b8a8b0dd084345e6c24b
fenix144 <yan_1731@hotmail.com>
2023-03-09 11:13:39 -0500

    Try stuff out

--- srcs/requirements/mariadb/config/entrypoint.sh
+++ srcs/requirements/mariadb/config/entrypoint.sh
@@ -3,15 +3,6 @@
 if [ -d "/var/run/mysqld" ]; then
 	echo "mysqld already present, skipping creation"
 
-<<<<<<< HEAD
-=======
-mysql_install_db --user=mysql --basedir=/usr/ --datadir=/var/lib/mysql
-
-exec mysqld --init-file=initfile.sql
-
-if [-f db_exists] then
-	echo "Database already configured..."
->>>>>>> 6690b96d01fcf74a565c123fdb43b0cde65a642a
 else
 	echo "mysqld not found, creating...."
 	mkdir -p /var/run/mysqld
