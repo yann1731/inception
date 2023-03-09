ba52ee0b6bef27c6058bd94d13d1475c36fd879b
fenix144 <yan_1731@hotmail.com>
2023-03-09 15:20:18 -0500

    Docker is kind of a nightmare without shake n bake images

--- srcs/requirements/mariadb/config/entrypoint.sh
+++ srcs/requirements/mariadb/config/entrypoint.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-if [ -d "/var/run/mysqld" ]; then
+if [ -f config ];
+then
 	echo "mysqld already present, skipping creation"
-
 else
 	echo "mysqld not found, creating...."
+
 	mkdir -p /var/run/mysqld
 	chown -R mysql:mysql /var/run/mysqld
-
+	echo USE mysql\; >> config
+	echo FLUSH PRIVILEGES\; >> config
+	echo DELETE FROM mysql.db WHERE User='' AND Host='%'\; >> config
+	echo GRANT ALL ON *.* TO 'root'@'localhost' identified by \'$MYSQL_ROOT_PASSWORD\' WITH GRANT OPTION\; >> config
+	echo SET PASSWORD FOR 'root'@'localhost'=PASSWORD\(\'${MYSQL_ROOT_PASSWORD}\'\)\; >> config
+	echo DROP DATABASE IF EXISTS test\; >> config
+	echo CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci\; >> config
+	echo GRANT ALL ON `$MYSQL_DATABASE`.* to \'$MYSQL_USER\'@'%' IDENTIFIED BY \'$MYSQL_PASSWORD\'\; >> config
 	mysqld --bootstrap < config
 fi
+
+exec $@
