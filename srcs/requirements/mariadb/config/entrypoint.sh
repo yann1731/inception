0807bcdc4f515d34de0660f15be37e53d2252152
fenix144 <yan_1731@hotmail.com>
2023-03-10 10:29:47 -0500

    let's go

--- srcs/requirements/mariadb/config/entrypoint.sh
+++ srcs/requirements/mariadb/config/entrypoint.sh
@@ -10,18 +10,15 @@ else
 
 	mkdir -p /var/run/mysqld
 	chown -R mysql:mysql /var/run/mysqld
-	adduser $MYSQL_USER
-	usermod -aG mysql $MYSQL_USER
-	mysql_install_db --user=$MYSQL_USER --basedir=/usr/ --datadir=/var/lib/mysql/
+	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --skip-auth-anonymous-user --skip-test-db
 	echo "USE mysql;" >> config
 	echo "FLUSH PRIVILEGES;" >> config
-	echo "DELETE FROM mysql.db WHERE User='' AND Host='%'\;" >> config
 	echo "GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> config
 	echo "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');" >> config
-	echo "DROP DATABASE IF EXISTS test;" >> config
-	echo "CREATE DATABASE IF NOT EXISTS `$MYSQL_DATABASE` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> config
-	echo "GRANT ALL ON `$MYSQL_DATABASE`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> config
-	echo "Fuck docker but here it is configuring mysqld using the bootstrap option"
+	echo "CREATE DATABASE IF NOT EXISTS '$WORDPRESS_DB_NAME' CHARACTER SET utf8 COLLATE utf8_general_ci;" >> config
+	echo "GRANT ALL ON '$WORDPRESS_DB_NAME'.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> config
+	echo "Done transferring to config..."
 	mysqld --bootstrap < config
 fi
-mysqld --user=$MYSQL_USER
+
+exec $@
