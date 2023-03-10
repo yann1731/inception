de382768ddabd63f2f44f9efd63e59fdd869a170
fenix144 <yan_1731@hotmail.com>
2023-03-10 11:38:58 -0500

    Passing mysqld_safe as argument to entrypoint

--- srcs/requirements/mariadb/config/entrypoint.sh
+++ srcs/requirements/mariadb/config/entrypoint.sh
@@ -10,17 +10,19 @@ else
 
 	mkdir -p /var/run/mysqld
 	chown -R mysql:mysql /var/run/mysqld
-	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --skip-auth-anonymous-user --skip-test-db
+	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --skip-test-db
 	echo "USE mysql;" >> config
 	echo "FLUSH PRIVILEGES;" >> config
+	echo "DELETE FROM mysql.db WHERE User='' AND Host='%'\;" >> config
 	echo "GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> config
 	echo "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');" >> config
+	echo "DROP DATABASE IF EXISTS test;" >> config
 	echo "CREATE DATABASE IF NOT EXISTS '$WORDPRESS_DB_NAME' CHARACTER SET utf8 COLLATE utf8_general_ci;" >> config
 	echo "GRANT ALL ON '$WORDPRESS_DB_NAME'.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> config
 	echo "Done transferring to config..."
 	mysqld --bootstrap < config
+	echo "Done configuring mysqld..."
 fi
 
-echo "Starting mysqld..."
-
-exec mysqld
+echo "Starting mysqld_safe..."
+exec $@
